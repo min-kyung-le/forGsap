83be32302347bbf6390a159c659cdb059776f2cc
mink <minkyung1435@gmail.com>
2023-03-16 09:55:00 +0900

    초기셋팅

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <img src="./assets/imgs/desk.jpg" alt="desk" class="desk" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+.desk {
+  width: 400px;
+  height: 600px;
+}
+</style>
